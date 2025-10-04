1d883a1c27cdbc1e6f42c24241a0693a16b19cc9
Coiby Xu <coiby.xu@gmail.com>
2025-10-04 12:38:38 +0800

    add tests

---
+++ tests/kab/test.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+# vim: dict+=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+set -x
+if [[ $TMT_TEST_RESTART_COUNT == 0 ]]; then
+	cd "$TMT_TREE" || exit 1
+	make install
+
+	KAB_SCRIPT=/usr/local/bin/kernel-auto-bisect/kab.sh
+	CONF_FILE=/usr/local/bin/kernel-auto-bisect/bisect.conf
+	TEST_SCRIPT=/usr/local/bin/kernel-auto-bisect/test.sh
+	KERNEL_RPM_LIST=/usr/local/bin/kernel-auto-bisect/kernel_list
+	GOOD_COMMIT=6.16.4-100.fc41.x86_64
+	BAD_COMMIT=6.16.7-100.fc41.x86_64
+
+	cat <<END >"$CONF_FILE"
+INSTALL_STRATEGY="rpm"
+TEST_STRATEGY="panic"
+REBOOT_STRATEGY=
+RPM_CACHE_DIR="/var/cache/kdump-bisect-rpms"
+GOOD_COMMIT=$GOOD_COMMIT
+BAD_COMMIT=$BAD_COMMIT
+REPRODUCER_SCRIPT=$TEST_SCRIPT
+KERNEL_RPM_LIST=$KERNEL_RPM_LIST
+END
+
+	cat <<END >$KERNEL_RPM_LIST
+https://kojipkgs.fedoraproject.org/packages/kernel/6.16.4/100.fc41/x86_64/kernel-core-6.16.4-100.fc41.x86_64.rpm
+https://kojipkgs.fedoraproject.org/packages/kernel/6.16.5/100.fc41/x86_64/kernel-core-6.16.5-100.fc41.x86_64.rpm
+https://kojipkgs.fedoraproject.org/packages/kernel/6.16.6/100.fc41/x86_64/kernel-core-6.16.6-100.fc41.x86_64.rpm
+https://kojipkgs.fedoraproject.org/packages/kernel/6.16.7/100.fc41/x86_64/kernel-core-6.16.7-100.fc41.x86_64.rpm
+END
+
+	cat <<END >"$TEST_SCRIPT"
+#!/bin/bash
+on_test() {
+    kernel_ver=\$(uname -r)
+    echo \$kernel_ver
+    if [[ \$kernel_ver == $GOOD_COMMIT ]]; then
+        return 0
+    elif [[ \$kernel_ver == $BAD_COMMIT ]]; then
+        return 1
+    else
+        return 0
+    fi
+}
+END
+
+	bash -x $KAB_SCRIPT </dev/null &>/root/test.log
+	# Make TMT wait for 120s the system to gets rebooted.
+	# CRIU may freeze kab.sh and tmt will regard the test as finished,
+	#  - If the system gets rebooted while TMT is collecting the test results,
+	#    TMT will abort with "plan failed".
+	#
+	#  - If the system gets rebooted after TMT finishes collecting the test
+	#    results, TMT will exit with "test passed"
+	sleep 120
+	echo "Something wrong, this line of code should never be reached"
+	exit 1
+else
+	GIT_REPO=/var/local/kernel-auto-bisect/git_repo
+	MAX_WAIT_TIME=600
+	wait_time=0
+	cd "$GIT_REPO" || exit 1
+
+	until git bisect log | grep -q "first bad commit" || [[ $wait_time -ge $MAX_WAIT_TIME ]]; do
+		sleep 5
+		wait_time=$((wait_time + 5))
+	done
+	if [[ $wait_time -ge $MAX_WAIT_TIME ]]; then
+		echo "Failed to get 1st bad commit"
+		exit 1
+	fi
+fi
