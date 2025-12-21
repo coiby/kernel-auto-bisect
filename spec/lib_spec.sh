72d2208b149568fa62659649e1e2774ed84ba869
Coiby Xu <coiby.xu@gmail.com>
2025-12-21 17:10:19 +0800

    Resolve error "grep: bad: No such file or directory"
    Currently, KAB will get stuck because it fails to tell bisecting should
    stop,
    
     + run_cmd -cwd /var/local/kernel-auto-bisect/git_repo git bisect log '|' grep -q 'first bad commit'
     + ssh -o BatchMode=yes 192.168.122.121 cd ''\''/var/local/kernel-auto-bisect/git_repo'\''' '&&' git bisect log '|' grep -q 'first bad commit'
     grep: bad: No such file or directory
    
    This happens because the quotes in the argument 'first bad commit'
    need to be escaped,
      # eval ls -d 'ab cd'
      ls: cannot access 'ab': No such file or directory
      ls: cannot access 'cd': No such file or directory
    
    However, run_cmd initialized no_escape to be true by mistake.
    
    After fixing this issue, another issue get unmasked. run_cmd escapes
    special characters like "|". For example, the following command will
    output "hi | grep ab",
     run_cmd echo hi "|" grep ab
    
    Fixes: 8084bad ("Resolve "cd: ': No such file or directory" error")
    Fixes: b658724 ("Run a command locally or remotely")

--- spec/lib_spec.sh
+++ spec/lib_spec.sh
@@ -2,24 +2,35 @@
 
 Describe 'kdump-lib'
 	Include ./lib.sh
-	KAB_SPEC_TEST_RUN_DIR=$(mktemp -u /tmp/spec_test.XXXXXXXXXX)
 
 	Describe "run_cmd"
 		out=hello
 		It "should handle cwd with a empty space corrently"
-			_dir_spac=${KAB_SPEC_TEST_RUN_DIR}/"a b"
-			mkdir -p "$_dir_spac"
-			When call run_cmd -cwd "$_dir_spac" echo "$out"
+			_dir_space=${SHELLSPEC_WORKDIR}/"a b"
+			mkdir -p "$_dir_space"
+			When call run_cmd -cwd "$_dir_space" echo "$out"
 			The output should equal $out
 		End
 
 		It "should handle a command with && correctly"
-			out1="hi"
-			_dir_spac=${KAB_SPEC_TEST_RUN_DIR}/"a b"
-			mkdir -p "$_dir_spac"
-			When call run_cmd -cwd "$_dir_spac" echo "$out1" "&&" echo "$out"
-			The output should include "$out1"
-			The output should include "$out"
+			out1="hi "
+			_dir_space=${SHELLSPEC_WORKDIR}/"a b"
+			mkdir -p "$_dir_space"
+			When call run_cmd -cwd "$_dir_space" echo -n "$out1" "&&" echo "$out"
+			The output should include "${out1}$out"
+		End
+
+		It "should handle a command with pipe correctly"
+			When call run_cmd echo "$out" "|" grep "$out"
+			The output should equal "$out"
+			The status should be success
+		End
+
+		It "should handle a command with spaces in the argument"
+			_str_with_space="ab cd"
+			When call run_cmd echo "$_str_with_space" "|" grep "$_str_with_space"
+			The output should equal "$_str_with_space"
+			The status should be success
 		End
 	End
 End
