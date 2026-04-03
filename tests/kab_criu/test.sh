af7e16c42d9b37301401520a6fdd21045bfed021
Coiby Xu <coiby.xu@gmail.com>
2026-04-03 20:17:37 +0800

    Refactor kab_criu test to use client/server topology and remove tmt dependencies
    When the kab_criu is running on a single machine, it's a headache to
    coordinate with tmt. For example, tmt can abort when the server reboots
    during CRIU-based bisection. And currently the ciru test plan still
    fail because it's waiting for "sleep $TMT_SLEEP_MARK" which will never
    happen.
    
    Based on the fact tmt won't care about the status of the machine once
    all tests are finished on a machine, update the `kab_criu` test to run
    on two machines (client and server) to avoid the above issues. This also
    allows us to completely decouple the core `kernel-auto-bisect` tool
    scripts (`lib.sh`, `kab.sh`, etc.) from tmt scheduling logic like
    `TMT_SLEEP_MARK` and `_wait_tmt_test`.

--- tests/kab_criu/test.sh
+++ tests/kab_criu/test.sh
@@ -2,21 +2,54 @@
 # vim: dict+=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
 set -x
 
-TMT_SLEEP_MARK=5.421379
+. ./tmt.sh
+
 [[ -z $ARCH ]] && ARCH=$(uname -m)
 
-if [[ $TMT_TEST_RESTART_COUNT == 0 ]]; then
-	cd "$TMT_TREE" || exit 1
-	make install
+if echo "${SERVERS}" | grep -qi "${HOSTNAME}"; then
+	exit 0
+fi
 
-	KAB_SCRIPT=/usr/local/bin/kernel-auto-bisect/kab.sh
-	CONF_FILE=/usr/local/bin/kernel-auto-bisect/bisect.conf
-	TEST_SCRIPT=/usr/local/bin/kernel-auto-bisect/test.sh
-	KERNEL_RPM_LIST=/usr/local/bin/kernel-auto-bisect/kernel_list
-	GOOD_COMMIT=6.16.4-100.fc41.${ARCH}
-	BAD_COMMIT=6.16.7-100.fc41.${ARCH}
+if ! echo "${CLIENTS}" | grep -qi "${HOSTNAME}"; then
+	echo "Something wrong, can't find test client"
+	exit 1
+fi
 
-	cat <<END >"$CONF_FILE"
+TARGET_HOST="${SERVERS}"
+
+TMT_TEST_PLAN_ROOT=${TMT_PLAN_DATA%data}
+SERVER_SSH_KEY=${TMT_TEST_PLAN_ROOT}/provision/server/id_ecdsa
+
+# ssh_cmd wrapper to handle local and remote execution
+ssh_cmd() {
+	local _opts="-o StrictHostKeyChecking=accept-new -o ConnectTimeout=10"
+	if [[ -f "$SERVER_SSH_KEY" ]]; then
+		_opts="$_opts -o IdentitiesOnly=yes -i $SERVER_SSH_KEY"
+	fi
+	ssh $_opts "$TARGET_HOST" "$@"
+	return $?
+}
+
+if ! ssh_cmd "cd $TMT_TREE && make install"; then
+	echo "Failed to install KAB on ${SERVERS}"
+	exit 1
+fi
+
+KAB_SCRIPT=/usr/local/bin/kernel-auto-bisect/kab.sh
+CONF_FILE=/usr/local/bin/kernel-auto-bisect/bisect.conf
+TEST_SCRIPT=/usr/local/bin/kernel-auto-bisect/test.sh
+KERNEL_RPM_LIST=/usr/local/bin/kernel-auto-bisect/kernel_list
+GIT_REPO=/var/local/kernel-auto-bisect/git_repo
+GOOD_COMMIT=6.16.4-100.fc41.${ARCH}
+BAD_COMMIT=6.16.7-100.fc41.${ARCH}
+
+# 1. Prepare Target
+echo "Waiting for target ($TARGET_HOST) to be ready..."
+until ssh_cmd "test -f $KAB_SCRIPT"; do
+	sleep 5
+done
+
+cat <<END | ssh_cmd "cat >$CONF_FILE"
 INSTALL_STRATEGY="rpm"
 TEST_STRATEGY="panic"
 REBOOT_STRATEGY=
@@ -25,17 +58,16 @@ GOOD_COMMIT=$GOOD_COMMIT
 BAD_COMMIT=$BAD_COMMIT
 REPRODUCER_SCRIPT=$TEST_SCRIPT
 KERNEL_RPM_LIST=$KERNEL_RPM_LIST
-TMT_SLEEP_MARK=$TMT_SLEEP_MARK
 END
 
-	cat <<END >$KERNEL_RPM_LIST
+cat <<END | ssh_cmd "cat >$KERNEL_RPM_LIST"
 https://kojipkgs.fedoraproject.org/packages/kernel/6.16.4/100.fc41/${ARCH}/kernel-core-6.16.4-100.fc41.${ARCH}.rpm
 https://kojipkgs.fedoraproject.org/packages/kernel/6.16.5/100.fc41/${ARCH}/kernel-core-6.16.5-100.fc41.${ARCH}.rpm
 https://kojipkgs.fedoraproject.org/packages/kernel/6.16.6/100.fc41/${ARCH}/kernel-core-6.16.6-100.fc41.${ARCH}.rpm
 https://kojipkgs.fedoraproject.org/packages/kernel/6.16.7/100.fc41/${ARCH}/kernel-core-6.16.7-100.fc41.${ARCH}.rpm
 END
 
-	cat <<END >"$TEST_SCRIPT"
+cat <<END | ssh_cmd "cat >$TEST_SCRIPT"
 #!/bin/bash
 on_test() {
     kernel_ver=\$(uname -r)
@@ -50,29 +82,31 @@ on_test() {
 }
 END
 
-	bash -x $KAB_SCRIPT </dev/null &>/root/test.log
-	# Make TMT wait for 120s the system to gets rebooted.
-	# CRIU may freeze kab.sh and tmt will regard the test as finished,
-	#  - If the system gets rebooted while TMT is collecting the test results,
-	#    TMT will abort with "plan failed".
-	#
-	#  - If the system gets rebooted after TMT finishes collecting the test
-	#    results, TMT will exit with "test passed"
-	sleep 120
-	echo "Something wrong, this line of code should never be reached"
-	exit 1
-else
-	GIT_REPO=/var/local/kernel-auto-bisect/git_repo
-	MAX_WAIT_TIME=600
-	wait_time=0
-	cd "$GIT_REPO" || exit 1
-
-	until git bisect log | grep -q "first bad commit" || [[ $wait_time -ge $MAX_WAIT_TIME ]]; do
-		sleep $TMT_SLEEP_MARK
-		wait_time=$((wait_time + SLEEP_TIME))
-	done
-	if [[ $wait_time -ge $MAX_WAIT_TIME ]]; then
-		echo "Failed to get 1st bad commit"
-		exit 1
-	fi
+# For idempotence
+ssh_cmd "rm -rf $GIT_REPO"
+# 2. Start kab.sh on Target if not already running and no checkpoint exists
+if ! ssh_cmd "pgrep -f $KAB_SCRIPT" >/dev/null 2>&1 && ! ssh_cmd "ls /var/local/kernel-auto-bisect/dump/core-*.img" >/dev/null 2>&1; then
+	echo "Starting kab.sh..."
+	ssh_cmd "setsid bash -x $KAB_SCRIPT </dev/null &>/root/test.log &"
 fi
+
+# 3. Wait for result
+MAX_WAIT_TIME=600 # 10 minutes
+wait_time=0
+while [[ $wait_time -lt $MAX_WAIT_TIME ]]; do
+	# Try to check if finished
+	output=$(ssh_cmd "git -C $GIT_REPO bisect log | grep 'first bad commit' | grep -q '$BAD_COMMIT'")
+	ret=$?
+
+	if [[ $ret -eq 0 ]]; then
+		exit 0
+	else
+		echo "Target ($TARGET_HOST) is down or unreachable (exit code: $ret), waiting..."
+	fi
+
+	sleep 10
+	wait_time=$((wait_time + 10))
+done
+
+echo "Failed to get 1st bad commit within $MAX_WAIT_TIME seconds"
+exit 1
